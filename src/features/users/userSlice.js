0196fe8d710f68393f442a1878b4886ad3413c64
VirgiawanLR <virgiawanlr@gmail.com>
2023-03-19 10:52:33 +0700

    add: login-functionality

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -7,15 +7,19 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     userList: [],
+    loginUser: {},
   },
   reducers: {
     setUserList: (state, action) => {
       state.userList = action.payload;
     },
+    setLoginUser: (state, action) => {
+      state.loginUser = action.payload;
+    },
   },
 });
 
-export const { setUserList } = userSlice.actions;
+export const { setUserList, setLoginUser } = userSlice.actions;
 
 export default userSlice.reducer;
 
@@ -37,6 +41,51 @@ export function fetchUserData() {
       dispatch(setUserList(response.data));
     } catch (error) {
       console.error(error);
+    }
+  };
+}
+
+export function loginCheck({ email, password }) {
+  return async (dispatch) => {
+    const options = {
+      method: "GET",
+      url: userAPI,
+      params: {
+        email,
+      },
+    };
+    try {
+      const response = await axios.request(options);
+      if (response.data.length) {
+        if (password === response.data[0].password) {
+          delete response.data[0].password;
+          dispatch(
+            setLoginUser({
+              ...response.data[0],
+              isSuccessed: true,
+              message: "success",
+            })
+          );
+        } else {
+          delete response.data[0].password;
+          dispatch(
+            setLoginUser({
+              ...response.data[0],
+              isSuccessed: false,
+              message: "email and password not match",
+            })
+          );
+        }
+      } else {
+        dispatch(
+          setLoginUser({
+            isSuccessed: false,
+            message: "email not yet register",
+          })
+        );
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 }
--- src/pages/auth/LoginForm.jsx
+++ src/pages/auth/LoginForm.jsx
@@ -1,16 +1,22 @@
 import { Field, Form, Formik } from "formik";
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { loginCheck } from "../../features/users/userSlice";
 
 function LoginForm() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const selector = useSelector((state) => state.user.loginUser);
   const onSubmit = (values, action) => {
-    console.log("values", values);
-    console.log("action", action);
+    dispatch(loginCheck(values));
+    action.resetForm();
   };
+  console.log("isSuccess", selector.isSuccessed);
+  console.log("message", selector.message);
+
   return (
-    <section class="dark:bg-gray-900 pt-20 px-4 lg:pt-32 secondary-font">
-      <div class="flex flex-col lg:flex-row items-center gap-8 justify-center px-6 pt-8 mx-auto lg:py-0 relative">
+    <section className="dark:bg-gray-900 pt-20 px-4 lg:pt-32 secondary-font">
+      <div className="flex flex-col lg:flex-row items-center gap-8 justify-center px-6 pt-8 mx-auto lg:py-0 relative">
         <div className="-mb-16 scale-105 lg:mb-0 lg:-mr-16 lg:scale-[1.15]">
           <img
             src={require("../../assets/images/login-img.avif")}
@@ -21,13 +27,18 @@ function LoginForm() {
           />
         </div>
         <div
-          class="w-11/12 md:w-full bg-white shadow-xl rounded-lg dark:border md:mt-0 z-50
+          className="w-11/12 md:w-full bg-white shadow-xl rounded-lg dark:border md:mt-0 z-50
           sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 pb-12"
         >
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-extrabold leading-tight text-dark md:text-2xl dark:text-white main-font tracking-normal">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-extrabold leading-tight text-dark md:text-2xl dark:text-white main-font tracking-normal">
               Sign in to your account
             </h1>
+            {selector.isSuccessed && selector.message ? null : (
+              <div className="text-red-600 text-xs px-1 font-semibold">
+                {selector.message}
+              </div>
+            )}
             <Formik
               initialValues={{
                 email: "",
@@ -37,11 +48,11 @@ function LoginForm() {
             >
               {(props) => {
                 return (
-                  <Form class="space-y-4 md:space-y-6">
+                  <Form className="space-y-4 md:space-y-6">
                     <div>
                       <label
-                        for="email"
-                        class="block mb-2 text-sm font-medium text-dark dark:text-white"
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-dark dark:text-white"
                       >
                         Your email
                       </label>
@@ -49,15 +60,15 @@ function LoginForm() {
                         type="email"
                         name="email"
                         id="email"
-                        class="bg-gray-50 border border-gray-300 text-dark sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-dark sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="name@company.com"
                         required=""
                       />
                     </div>
                     <div>
                       <label
-                        for="password"
-                        class="block mb-2 text-sm font-medium text-dark dark:text-white"
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-dark dark:text-white"
                       >
                         Password
                       </label>
@@ -66,25 +77,25 @@ function LoginForm() {
                         name="password"
                         id="password"
                         placeholder="••••••••"
-                        class="bg-gray-50 border border-gray-300 text-dark sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-dark sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required=""
                       />
                     </div>
-                    <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                        <div class="flex items-center h-5">
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                        <div className="flex items-center h-5">
                           <input
                             id="remember"
                             aria-describedby="remember"
                             type="checkbox"
-                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary dark:ring-offset-gray-800"
+                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary dark:ring-offset-gray-800"
                             required=""
                           />
                         </div>
-                        <div class="ml-3 text-sm">
+                        <div className="ml-3 text-sm">
                           <label
-                            for="remember"
-                            class="text-gray-500 dark:text-gray-300"
+                            htmlFor="remember"
+                            className="text-gray-500 dark:text-gray-300"
                           >
                             Remember me
                           </label>
@@ -92,22 +103,22 @@ function LoginForm() {
                       </div>
                       <a
                         href="#"
-                        class="text-sm font-medium text-primary hover:underline dark:text-primary"
+                        className="text-sm font-medium text-primary hover:underline dark:text-primary"
                       >
                         Forgot password?
                       </a>
                     </div>
                     <button
                       type="submit"
-                      class="w-full text-white bg-primary hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-teal-800 dark:focus:ring-primary"
+                      className="w-full text-white bg-primary hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-teal-800 dark:focus:ring-primary"
                     >
                       Sign in
                     </button>
-                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet?{" "}
                       <button
                         onClick={() => navigate("/register")}
-                        class="font-medium text-primary hover:underline dark:text-primary"
+                        className="font-medium text-primary hover:underline dark:text-primary"
                       >
                         Sign up
                       </button>
